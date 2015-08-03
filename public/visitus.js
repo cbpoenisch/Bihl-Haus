74302bcaba16d8acd0215b5f0d4fb65d2d05c88d
Crystal Poenisch <crystalpoenisch@Crystal-Poenischs-MacBook-Pro.local>
2015-08-02 23:30:09 -0500

    style the visit-us and support-us pages a bit. Moved the google maps js to an external file.

---
+++ public/visitus.js
@@ -0,0 +1,25 @@
+<script type="text/javascript">
+    (function() {
+        "use strict";
+        
+        var mapOptions = {
+            zoom: 15,
+            center: {
+               lat: 29.469665,
+               lng: -98.531612
+            }
+        };
+
+        // Render the map
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+        var bihlHaus = { lat: 29.469665, lng: -98.531612};
+
+		var marker = new google.maps.Marker({
+	    	position: bihlHaus,
+	    	map: map
+		});
+	})();
+
+    
+    </script>
